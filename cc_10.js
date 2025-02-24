23961b8539265b3fec92100a8c753faa56987539
mvivy3 <meredithyen@usf.edu>
2025-02-23 21:58:28 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -21,3 +21,26 @@ console.log(prod1.getDetails());
 
 prod1.updateStock(3);
 console.log(prod1.getDetails());
+
+// TASK 2: CREATING AN ORDER CLASS
+
+// Create a class Order with the following properties
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+        if (this.product.stock >= this.quantity) {
+            this.product.updateStock(this.quantity);
+        } else {
+            console.log("error");
+        }
+    }
+    getOrderDetails() { // Add a method getOrderDetails() that returns order details
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${this.quantity * this.product.price}`;
+    }
+}
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails());
+
+console.log(prod1.getDetails());
